7c3f7f2c879bb0d8ab0f27798027a9456eeebf44
Erik Geiser <erik.geiser@posteo.net>
2021-07-30 21:37:47 +0200

    Add doc string for promptkit package

--- promptkit.go
+++ promptkit.go
@@ -1,1 +1,6 @@
+/*
+Package promptkit is a collection of common command line prompts for interactive
+programs. Each prompts comes with sensible defaults, re-mappable key bindings
+and many opportunities for heavy customization.
+*/
 package promptkit
